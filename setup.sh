d8349ece8794ec92173fd264a108bae05d5cb88d
denhampreen <denhampreen@gmail.com>
2020-08-30 16:19:53 +0200

    setup files

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Install apps and binaries with Brew
+source ./brew.sh
+
+# Create symlinks for dotfiles
+source ./link-dotfiles.sh
+
+# Configure MacOS defaults.
+# You only want to run this once during setup. Additional runs may reset changes you make manually.
+source ./macos
